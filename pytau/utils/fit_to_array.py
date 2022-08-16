bcd0550d6f21c197bc222a1260ebc082c57d5c4b
abuzarmahmood <abuzarmahmood@gmail.com>
2022-08-16 12:13:02 -0400

    Script to fit using np array instead of HDF5 file

---
+++ pytau/utils/fit_to_array.py
@@ -0,0 +1,61 @@
+"""
+Fit models to given spike array
+"""
+
+import json
+import os
+import pickle
+import shutil
+import uuid
+from datetime import date, datetime
+from glob import glob
+from tqdm import tqdm, trange
+import seaborn as sns
+import pingouin as pg
+import sys
+
+import numpy as np
+import pandas as pd
+
+import sys
+base_dir = '/media/bigdata/projects/pytau'
+sys.path.append(base_dir)
+
+from pytau import changepoint_model
+from pytau import changepoint_preprocess
+from pytau.utils import EphysData
+
+import pymc3
+import theano
+
+
+for ind in trange(len(quin_clust_flat)):
+    data = quin_clust_flat[ind] 
+    model_parameters_keys = ['states','fit','samples', 'model_kwargs']
+    model_parameters_values = [4, 80000, 20000, {'None':None}]
+    preprocess_parameters_keys = ['time_lims','bin_width','data_transform']
+    preprocess_parameters_values = [[2000,4000], 50, None]
+
+    model_params = dict(zip(model_parameters_keys, model_parameters_values))
+    preprocess_params = dict(zip(preprocess_parameters_keys, 
+        preprocess_parameters_values))
+
+    preprocessor = changepoint_preprocess.preprocess_single_taste
+    model_template= changepoint_model.single_taste_poisson
+    inference_func = changepoint_model.advi_fit
+
+    preprocessed_data = preprocessor(data, **preprocess_params)
+
+    model = model_template(preprocessed_data,
+                                 model_params['states'],
+                                 **model_params['model_kwargs'])
+
+    temp_outs = inference_func(model,
+                        model_params['fit'],
+                        model_params['samples'])
+    varnames = ['model', 'approx', 'lambda', 'tau', 'data']
+    inference_outs = dict(zip(varnames, temp_outs))
+
+    tau_array = inference_outs['tau']
+    tau_hist_array = convert_to_hist_array(tau_array, bins)
+    np.save(os.path.join(save_path, id_frame['fin_name'][ind]), tau_hist_array)
