1401ec5e36420229d4d39214e27c44edca4515e6
abuzarmahmood <abuzarmahmood@gmail.com>
2023-03-23 15:23:13 -0400

    Update how to with using saved models database

--- pytau/how_to/fit_with_fit_handler.py
+++ pytau/how_to/fit_with_fit_handler.py
@@ -55,3 +55,39 @@ this_handler = PklHandler('/path/to/pkl/file')
 # Mode Scale Tau : Mode of Int Scaled Tau ==> this_handler.tau.scaled_mode_tau
 
 # Can also get path to pkl file from model database
+from pytau.changepoint_io import DatabaseHandler
+fit_database = DatabaseHandler()
+fit_database.drop_duplicates()
+fit_database.clear_mismatched_paths()
+
+# Get fits for a particular experiment
+dframe = fit_database.fit_database
+wanted_exp_name = 'bla_population_elbo_repeat'
+wanted_frame = dframe.loc[dframe['exp.exp_name'] == wanted_exp_name] 
+# Pull out a single data_directory
+data_dir = wanted_frame['data.data_dir'].iloc[0]
+
+## Information saved in model database 
+# preprocess.time_lims
+# preprocess.bin_width
+# preprocess.data_transform
+# preprocess.preprocessor_name
+# model.states
+# model.fit
+# model.samples
+# model.model_kwargs
+# model.model_template_name
+# model.inference_func_name
+# data.data_dir
+# data.basename
+# data.animal_name
+# data.session_date
+# data.taste_num
+# data.laser_type
+# data.region_name
+# exp.exp_name
+# exp.model_id
+# exp.save_path
+# exp.fit_date
+# module.pymc3_version
+# module.theano_version
